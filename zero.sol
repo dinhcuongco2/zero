a494653ab3e18b7c3172081f23afc8284e7204cd
dinhcuong.com2 <158152159+dinhcuongco2@users.noreply.github.com>
2026-05-31 17:04:10 +0200

    zero.sol

--- zero.sol
+++ zero.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract zero {
+
+    string public name = "zero";
+    string public symbol = "zero";
+
+    uint256 public totalSupply = 1000000000;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+}
